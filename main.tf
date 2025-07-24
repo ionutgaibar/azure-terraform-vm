59d128dc700e1bf44f92338c93b72cc1834ee34e
krystian334 <ionutgamer55@gmail.com>
2025-07-24 10:46:58 +0300

    Second Attempt

--- main.tf
+++ main.tf
@@ -26,7 +26,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "vm-public-ip-${count.index}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "nic" {
